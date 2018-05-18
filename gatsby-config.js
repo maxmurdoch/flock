1d370bee0b81d89b5518998b8296e45d73b72fc3
Max Murdoch <max@maxmurdo.ch>
2018-05-18 15:57:22 +0100

    chore: initiate plugins in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Flock',
+    description: 'Cover',
+    keywords: 'insurance, drones, pay-as-you-go',
   },
-  plugins: ['gatsby-plugin-netlify-cms', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/images`,
+        name: 'images',
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify-cms',
+  ],
 }
