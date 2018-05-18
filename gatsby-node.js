fac00cad0b1dfdcd2da339d126770b034f56af1c
Max Murdoch <max@maxmurdo.ch>
2018-05-18 15:57:52 +0100

    chore: add basic gatsby node apis

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,71 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+const R = require('ramda')
+const { kebabCase } = require('voca')
+const path = require('path')
+const { createFilePath } = require('gatsby-source-filesystem')
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  return graphql(`
+    {
+      allMarkdownRemark(limit: 1000) {
+        edges {
+          node {
+            id
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+              templateKey
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      result.errors.forEach(e => console.error(e.toString()))
+      return Promise.reject(result.errors)
+    }
+
+    const markdownPages = result.data.allMarkdownRemark.edges
+    console.log(markdownPages)
+
+    markdownPages.forEach(edge => {
+      const {
+        id,
+        frontmatter: { title },
+      } = edge.node
+      createPage({
+        path: edge.node.fields.slug,
+        component: path.resolve(
+          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        ),
+        // additional data can be passed via context
+        context: {
+          title,
+          id,
+        },
+      })
+    })
+  })
+}
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
