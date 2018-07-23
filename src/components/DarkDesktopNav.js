df56ab7b215c0c0b0e33e668394c81e26bd0ffa6
Abraham Polishchuk <apolishc@gmail.com>
2018-07-23 22:21:03 +0000

    add segment on download buttons

--- src/components/DarkDesktopNav.js
+++ src/components/DarkDesktopNav.js
@@ -7,8 +7,9 @@ import blackLogo from '../images/logo-black.svg'
 import blackDownArrow from '../images/icons/small-arrow-black.svg'
 import Nav from './Nav'
 import {colors} from '../constants/theme'
+import trackDownload from './utils/trackDownload'
 
-const LightDesktopNav = () => {
+const DarkDesktopNav = () => {
   return (
     <Nav
       textColor={() => colors.dark}
@@ -17,10 +18,10 @@ const LightDesktopNav = () => {
         <img className={css({width: '6rem', margin: 0})} src={blackLogo} />
       )}
       DownloadButton={() => (
-        <SecondaryButton Text={SmallText}>Download</SecondaryButton>
+        <SecondaryButton Text={SmallText} onClick={trackDownload}>Download</SecondaryButton>
       )}
     />
   )
 }
 
-export default LightDesktopNav
+export default DarkDesktopNav
---
+++ src/utils/trackDownload.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const downloadClickHandler = () => {
+  analytics.track('Download Clicked')
+}
+
+export default downloadClickHandler
