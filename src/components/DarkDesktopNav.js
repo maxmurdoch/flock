e33871da4a8e78d5b48f775eea936e676c2f7109
Abraham Polishchuk <apolishc@gmail.com>
2018-07-24 10:32:37 +0000

    fix build filepath

--- src/components/DarkDesktopNav.js
+++ src/components/DarkDesktopNav.js
@@ -7,7 +7,7 @@ import blackLogo from '../images/logo-black.svg'
 import blackDownArrow from '../images/icons/small-arrow-black.svg'
 import Nav from './Nav'
 import {colors} from '../constants/theme'
-import trackDownload from './utils/trackDownload'
+import trackDownload from '../utils/trackDownload'
 
 const DarkDesktopNav = () => {
   return (
