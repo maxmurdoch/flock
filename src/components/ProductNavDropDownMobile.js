0b7f3b0b1338051394630d4e24915376cf2db57d
mattcleary55 <mattcleary55@gmail.com>
2019-02-20 13:32:58 +0000

    refactored mobile product nav drop down

--- src/components/ProductNavDropDownMobile.js
+++ src/components/ProductNavDropDownMobile.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import R from 'ramda'
-import {css} from 'react-emotion'
-import {Collapse} from 'react-collapse'
 
 import SiteContainer from './SiteContainer'
 import Flex from './Flex'
-import Box from './Box'
 import PrimaryButton from './NavPrimaryButton'
 import SecondaryButton from './NavSecondaryButton'
-import ProductLink from './ProductLink'
-import {colors, breakpoints} from '../constants/theme'
+import {colors} from '../constants/theme'
 
 const mapIndex = R.addIndex(R.map)
 
@@ -22,46 +18,25 @@ const ProductNavDropDown = () => {
       pt={30}
     >
       <SiteContainer edgeToEdge>
-
-          {mapIndex(
-            ({to, text, options, hasIcon}, index) => (
-              <Flex
-                mb="20px"
-                key={index}
-                flexDirection="column"
-                className={css({
-                  // [`@media (min-width: ${R.nth(0, breakpoints)})`]: {
-                  //   width: '50%'
-                  // },
-
-                  // '@media (min-width: 1200px)': {
-                  //   width: '25%'
-                  // }
-                })}
-              >
-                <Flex flexDirection="column">
-                  <PrimaryButton
+        {mapIndex(
+          ({to, text, options, hasIcon}, index) => (
+            <Flex flexDirection="column" mb="20px" key={index}>
+              <PrimaryButton to={to} title={text} hasIcon={hasIcon} />
+              {mapIndex(
+                ({to, text, icon}) => (
+                  <SecondaryButton
                     to={to}
                     title={text}
-                    hasIcon={hasIcon}
+                    flexGrow={1}
+                    icon={icon}
                   />
-                  {mapIndex(
-                    ({to, text, icon}) => (
-                      <SecondaryButton
-                        to={to}
-                        title={text}
-                        flexGrow={1}
-                        icon={icon}
-                      />
-                    ),
-                    options
-                  )}
-                </Flex>
-              </Flex>
-            ),
-            buttonContent
-          )}
-
+                ),
+                options
+              )}
+            </Flex>
+          ),
+          buttonContent
+        )}
       </SiteContainer>
     </Flex>
   )
